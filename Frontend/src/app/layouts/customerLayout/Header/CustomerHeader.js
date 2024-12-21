fa6c62aca3d4ac15ddada5078e5e3e13115ee88c
Vu Binh <22520129@gm.uit.edu.vn>
2024-12-21 17:36:06 +0700

    Customer: Hoàn thành Header

---
+++ Frontend/src/app/layouts/customerLayout/Header/CustomerHeader.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "../../../styles/Customer/CustomerLayout/CustomerHeader.css";
+import logo from "../../../assets/images/logo_phongkham.png";
+
+export default function CustomerHeader() {
+  return (
+    <header className="customer-header">
+      <div className="logo">
+        <img className="logo-img" src={logo} alt="Logo phòng khám"></img>
+      </div>
+      <nav className="header-nav">
+        <ul className="nav-list">
+          <li className="nav-item">
+            <a></a>Trang chủ
+          </li>
+          <li className="nav-item dropdown">
+            <a href="#" className="nav-link">
+              Dịch vụ
+            </a>
+            <ul className="dropdown-menu">
+              <li>
+                <a href="/service1" className="dropdown-item">
+                  Đăng kí khám
+                </a>
+              </li>
+              <li>
+                <a href="/service2" className="dropdown-item">
+                  Bảng giá dịch vụ khám
+                </a>
+              </li>
+            </ul>
+          </li>
+
+          <li className="nav-item">
+            <a href=""></a>Bác sĩ
+          </li>
+          <li className="nav-item">
+            <a></a>Bài viết
+          </li>
+          <li className="nav-item" id="btn-dangnhap">
+            <a></a>Đăng nhập
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
