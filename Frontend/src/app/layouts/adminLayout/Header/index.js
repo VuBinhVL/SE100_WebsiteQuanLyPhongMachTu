56dc543f06e9b8007904d5397105e465a00b1be9
Tran Tien Dat <ttdat260204@gmail.com>
2024-12-23 00:56:34 +0700

    complete static LayoutAdmin

--- Frontend/src/app/layouts/adminLayout/Header/index.js
+++ Frontend/src/app/layouts/adminLayout/Header/index.js
@@ -1,5 +1,19 @@
 import React from "react";
-
+import { FaUserCircle } from "react-icons/fa";
+import "../../../styles/adminStyle/Header/Header.css"
 export default function Header() {
-    return <div>Header</div>;
+    return (
+        <>
+            <div className="section_Left fs-3">
+                Quản lý thuốc
+            </div>
+            <div className="section_Right d-flex align-items-center">
+                <a href="#" className="fs-3 d-flex align-items-center">
+                    <FaUserCircle className="fs-3 icon_header" />
+                </a>
+            </div>
+        </>
+
+
+    );
 }
--- Frontend/src/app/routes/MainRoutes.js
+++ Frontend/src/app/routes/MainRoutes.js
@@ -2,20 +2,17 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Admin/Home/Home";
 import PageNotFound from "../layouts/adminLayout/PageNotFound";
-import Header from "../layouts/adminLayout/Header/index";
-import Footer from "../layouts/adminLayout/Footer/Footer";
-import Navigation from "../layouts/adminLayout/Navigation";
+import LayoutAdmin from "../layouts/adminLayout";
 
 export default function MainRoutes() {
   return (
     <BrowserRouter>
-      {/* <Header /> */}
-      <Navigation />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<PageNotFound />} />
+        <Route path="/" element={<LayoutAdmin />}>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Route>
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 }
