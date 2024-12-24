cd0ea06e258d48043ffcdce8d02863020216169e
Dung Tien The <dungtienthe1920@gmail.com>
2024-12-24 22:51:24 +0700

    BE-Common: Add ResponeMessage,HttpStatusCode

---
+++ Backend/PhongMachTu/PhongMachTu.Common/ConstValue/HttpStatusCode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhongMachTu.Common.ConstValue
+{
+	public static class HttpStatusCode
+	{
+		public static readonly int Ok = 200;
+
+		public static readonly int BadRequest = 400;
+
+		public static readonly int Unauthorized = 401;
+
+		public static readonly int NotFound = 404;
+
+		public static readonly int InternalServerError = 500;
+
+	}
+}
