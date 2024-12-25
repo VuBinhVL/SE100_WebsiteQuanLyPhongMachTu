9965dc75ec5474587df0cd54178d46f25aaf2e2e
Dung Tien The <dungtienthe1920@gmail.com>
2024-12-26 02:01:18 +0700

    BE-Quản lý loại xét nghiệm: xây dựng api trả về list loại xét nghiệm đang có

---
+++ Backend/PhongMachTu/PhongMachTu.Service/LoaiXetNghiemService.cs
@@ -0,0 +1,30 @@
+﻿using PhongMachTu.DataAccess.Models;
+using PhongMachTu.DataAccess.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhongMachTu.Service
+{
+    public interface ILoaiXetNghiemService
+    {
+        Task<List<LoaiXetNghiem>> GetAllAsync();
+    }
+
+    public class LoaiXetNghiemService : ILoaiXetNghiemService
+    {
+        private readonly ILoaiXetNghiemRepository _loaiXetNghiemRepository;
+        public LoaiXetNghiemService(ILoaiXetNghiemRepository loaiXetNghiemRepository)
+        {
+            _loaiXetNghiemRepository = loaiXetNghiemRepository;
+        }
+
+        public async Task<List<LoaiXetNghiem>> GetAllAsync()
+        {
+            var rs = await _loaiXetNghiemRepository.GetAllWithIncludeAsync(l=>l.DonViTinh);
+            return rs.ToList();
+        }
+    }
+}
---
+++ Backend/PhongMachTu/PhongMachTu.WebAPI/Areas/Admin/LoaiXetNghiemController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PhongMachTu.Common.ConstValue;
+using PhongMachTu.DataAccess.Models;
+using PhongMachTu.Service;
+
+namespace PhongMachTu.WebAPI.Areas.Admin
+{
+    [Area("ADMIN")]
+    [Route("api/quan-li-loai-xet-nghiem")]
+    [ApiController]
+    public class LoaiXetNghiemController : ControllerBase
+    {
+        private readonly ILoaiXetNghiemService _loaiXetNghiemService;
+        public LoaiXetNghiemController(ILoaiXetNghiemService loaiXetNghiemService)
+        {
+            _loaiXetNghiemService = loaiXetNghiemService;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            try
+            {
+                var rs = await _loaiXetNghiemService.GetAllAsync();
+                return StatusCode(HttpStatusCode.Ok, rs);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(HttpStatusCode.InternalServerError, new { message = HttpStatusCode.MsgHeThongGapSuCo });
+            }
+
+        }
+    }
+}
