0befba89842f939e50229d9750f4ccb700299f42
Vu Binh <22520129@gm.uit.edu.vn>
2024-12-27 23:07:42 +0700

    Fix merge

--- Frontend/src/app/routes/MainRoutes.js
+++ Frontend/src/app/routes/MainRoutes.js
@@ -1,25 +1,46 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PageNotFound from "../layouts/adminLayout/PageNotFound";
+import LayoutAdmin from "../layouts/adminLayout";
+import Staff from "../pages/Admin/UsersManagement/Staffs";
+import DashBoard from "../pages/Admin/DashBoard";
+import Patien from "../pages/Admin/UsersManagement/Patiens";
+import ExaminationForm from "../pages/Admin/ExaminationFormsManagement";
+import MedicalShift from "../pages/Admin/MedicalShiftsManagement";
+import Medicine from "../pages/Admin/MedicinesManagement";
 import CustomerFooter from "../layouts/customerLayout/Footer";
 import CustomerHeader from "../layouts/customerLayout/Header/CustomerHeader";
 import CustomerHome from "../pages/Customer/CustomerHome";
 import Doctor from "../pages/Customer/Doctor";
 import Register from "../pages/Other/Register/Register";
 import Login from "../pages/Other/Login";
+import LayoutCustomer from "../layouts/customerLayout";
 
 export default function MainRoutes() {
   return (
     <BrowserRouter>
-      <CustomerHeader />
       <Routes>
-        <Route path="/" element={<CustomerHome />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        {/* Layout khách hàng */}
+        <Route path="/" element={<LayoutCustomer />}>
+          <Route index element={<CustomerHome />} />
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+          <Route path="doctors" element={<Doctor />} />
+        </Route>
+
+        {/* Layout quản trị viên */}
+        <Route path="/admin" element={<LayoutAdmin />}>
+          <Route index element={<DashBoard />} />
+          <Route path="staff" element={<Staff />} />
+          <Route path="patien" element={<Patien />} />
+          <Route path="examinationform" element={<ExaminationForm />} />
+          <Route path="medicalshift" element={<MedicalShift />} />
+          <Route path="medicine" element={<Medicine />} />
+        </Route>
+
+        {/* Trang không tìm thấy */}
         <Route path="*" element={<PageNotFound />} />
-        <Route path="/doctors" element={<Doctor />} />
       </Routes>
-      <CustomerFooter />
     </BrowserRouter>
   );
 }
