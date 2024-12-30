2c1473355f017fe7eb0cdfc305d6ff6ad75f978c
vanminh1878 <vanminh130804@gmail.com>
2024-12-30 16:24:59 +0700

    chạy lại csdl

---
+++ Backend/PhongMachTu/PhongMachTu.DataAccess/Migrations/20241229114528_InitDbVer1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PhongMachTu.DataAccess.Migrations
+{
+    public partial class InitDbVer1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
