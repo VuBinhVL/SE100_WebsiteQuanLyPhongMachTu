a674c2580110f47989096bc4b897ff954d22fb64
vanminh1878 <vanminh130804@gmail.com>
2025-01-01 17:14:16 +0700

    BE: xây API hiển thị danh sách bệnh nhân

---
+++ Backend/PhongMachTu/PhongMachTu.Common/DTOs/Request/NguoiDung/Request_HienThiDanhSachBenhNhan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhongMachTu.Common.DTOs.Request.NguoiDung
+{
+    public class Request_HienThiDanhSachBenhNhan
+    {
+        public List<BenhNhanDTO> BenhNhanList { get; set; } = new List<BenhNhanDTO>();
+
+    }
+    public class BenhNhanDTO
+    {
+       
+        public string? HoTen { get; set; }
+        public string? GioiTinh { get; set; }
+        public int? Tuoi { get; set; }
+        public string? SoDienThoai { get; set; }
+        public string? DiaChi { get; set; }
+    }   
+}
---
+++ Backend/PhongMachTu/PhongMachTu.WebAPI/Areas/Admin/BenhNhanController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PhongMachTu.Service;
+
+namespace PhongMachTu.WebAPI.Areas.Admin
+{
+    [Area("ADMIN")]
+    [Route("api/admin/quan-li-benh-nhan")]
+    [ApiController]
+    public class BenhNhanController : ControllerBase
+    {
+        private readonly INguoiDungService _nguoiDungService;
+        public BenhNhanController(INguoiDungService nguoiDungService)
+        {
+            _nguoiDungService = nguoiDungService;
+        }
+        [HttpGet("hien-thi-danh-sach-benh-nhan")]
+        public async Task<IActionResult> HienThiDanhSachBenhNhanAsync()
+        {
+            try
+            {
+                var rs = await _nguoiDungService.HienThiDanhSachBenhNhanAsync();
+                return StatusCode(StatusCodes.Status200OK, rs);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+    }
+}
