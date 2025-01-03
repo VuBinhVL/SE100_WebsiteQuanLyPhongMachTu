c00983b908141806743f96aa6e1f8f79c99bedaa
Dung Tien The <dungtienthe1920@gmail.com>
2025-01-03 17:29:06 +0700

    BE-Thêm khóa ngoại NhomBenh để cho biết CaKham khám về nhóm bệnh nào

---
+++ Backend/PhongMachTu/PhongMachTu.DataAccess/Migrations/20250103102534_Them_Attribute_NhomBenh_Cho_CaKham.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PhongMachTu.DataAccess.Migrations
+{
+    public partial class Them_Attribute_NhomBenh_Cho_CaKham : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NhomBenhId",
+                table: "CaKhams",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CaKhams_NhomBenhId",
+                table: "CaKhams",
+                column: "NhomBenhId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CaKhams_NhomBenhs_NhomBenhId",
+                table: "CaKhams",
+                column: "NhomBenhId",
+                principalTable: "NhomBenhs",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CaKhams_NhomBenhs_NhomBenhId",
+                table: "CaKhams");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CaKhams_NhomBenhId",
+                table: "CaKhams");
+
+            migrationBuilder.DropColumn(
+                name: "NhomBenhId",
+                table: "CaKhams");
+        }
+    }
+}
