d5f9545bb3422d15bd92538f628e57d1af0eb27f
Tran Tien Dat <ttdat260204@gmail.com>
2025-01-09 13:12:52 +0700

    xong ui chi tiết phiếu khám bệnh

--- Frontend/src/app/components/Admin/ExaminationFormManagement/DeleteExaminationForm/DeleteExaminationForm.js
+++ Frontend/src/app/components/Admin/ExaminationFormManagement/DeleteExaminationForm/DeleteExaminationForm.js
@@ -4,28 +4,35 @@ import { MdDelete } from "react-icons/md";
 import { fetchDelete } from "../../../../lib/httpHandler";
 import { showSuccessMessageBox } from "../../../MessageBox/SuccessMessageBox/showSuccessMessageBox";
 import { showDeleteMessageBox } from "../../../MessageBox/DeleteMesssageBox/showDeleteMessageBox";
+import { showErrorMessageBox } from "../../../MessageBox/ErrorMessageBox/showErrorMessageBox";
 function DeleteExaminationForm(props) {
     // const [isVisibility, setIsVisibility] = useState(false);
     const { setListExaminationForm, listExaminationForm } = props;
     const { item } = props;
     const handleDelete = () => {
+        const status = "Hoàn tất"
         showDeleteMessageBox("Bạn có chắc muốn xóa không", () => {
-            const uri = `/api/admin/quan-li-benh-nhan/delete?id=${item.id}`;
-            fetchDelete(
-                uri, "",
-                (sus) => {
-                    // console.log(">>>>>>.check sus", sus)
-                    showSuccessMessageBox(sus.message)
-                    const newlistExaminationForm = listExaminationForm.filter((newItem) => newItem.id !== item.id);
-                    setListExaminationForm(newlistExaminationForm)
-                },
-                (fail) => {
-                    alert(fail.message);
-                },
-                () => {
-                    alert("Có lỗi xảy ra");
-                }
-            );
+            if (item.tenTrangThaiPKB.trim().toLowerCase() === status.trim().toLocaleLowerCase()) {
+                showErrorMessageBox("Không thể xóa phiếu khám này vì nó đã thanh toán")
+                return;
+            }
+            // const uri = `/api/admin/quan-li-benh-nhan/delete?id=${item.id}`;
+            // fetchDelete(
+            //     uri, "",
+            //     (sus) => {
+            //         // console.log(">>>>>>.check sus", sus)
+            //         showSuccessMessageBox(sus.message)
+            //         const newlistExaminationForm = listExaminationForm.filter((newItem) => newItem.id !== item.id);
+            //         setListExaminationForm(newlistExaminationForm)
+            //     },
+            //     (fail) => {
+            //         alert(fail.message);
+            //     },
+            //     () => {
+            //         alert("Có lỗi xảy ra");
+            //     }
+            // );
+            showSuccessMessageBox("Xóa thành công")
         });
     }
     return (
